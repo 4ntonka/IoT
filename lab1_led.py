84946ff68fbf1455db327e97bb65214755fb71f0
Nathaniel <nathaniel.kamperv@gmail.com>
2025-06-02 14:43:58 +0200

    correct port

--- lab1_led.py
+++ lab1_led.py
@@ -7,7 +7,7 @@ import serial
 import time
 
 # Serial port configuration
-PORT = '/dev/ttyAMC0'  # Adjust this based on your system
+PORT = '/dev/ttyACM0'  # Adjust this based on your system
 BAUD_RATE = 9600
 
 # LED commands
