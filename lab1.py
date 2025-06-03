f57c9078c86b918070ee130696d012647f0d30f2
Nathaniel <nathaniel.kamperv@gmail.com>
2025-06-03 11:06:18 +0200

    spinbox features

--- lab1.py
+++ lab1.py
@@ -17,17 +17,21 @@ class Lab1(QDialog):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
         self.setWindowTitle("arduino_sensors")
-        self.remaining_time = 10
-
-        self.x = list(range(1, 11)) 
-        self.y = [random.uniform(0, 1) for _ in self.x] 
+        self.remaining_time = 11
+        interval = self.ui.interval.value()
+        self.max_x = self.ui.maxxaxis.value()
+        self.x = list(range(1, 11))
+        self.y = [random.uniform(0, 1) for _ in self.x]
 
         self.ui.pushButton.clicked.connect(self.toggle_timer)
         self.timer = QTimer()
-        self.timer.setInterval(500)  # 500 ms = 0,5 seconden
+        self.timer.setInterval(interval)  # 500 ms = 0,5 seconden
         self.timer.timeout.connect(self.mybuttonfunction)
         # Display initial plot when application starts
         self.mybuttonfunction()
+        self.ui.interval.valueChanged.connect(self.update_timer_interval)
+        self.ui.maxxaxis.valueChanged.connect(self.update_max_xaxis)
+
 
     def mybuttonfunction(self):
         if self.remaining_time > 0:
@@ -35,27 +39,40 @@ class Lab1(QDialog):
             self.ui.label_timer.setText(f"{self.remaining_time} seconds")
         if self.remaining_time == 0:
             self.timer.stop()
-        # Update plot (optioneel)
+            self.ui.pushButton.setText("Start")
+            self.remaining_time = 10
+            self.ui.label_timer.setText(f"{self.remaining_time} seconds")
         new_value = random.uniform(0, 1)
         self.y.append(new_value)
-        if len(self.y) > 10:
+        if len(self.y) > self.max_x:
             self.y.pop(0)
+
 
         self.ui.MplWidget.canvas.axes.clear()
         self.ui.MplWidget.canvas.axes.plot(self.x, self.y, 'r', linewidth=0.5)
         self.ui.MplWidget.canvas.draw()
 
     def update_random(self):
-        self.y = [random.uniform(0, 1) for _ in self.x] 
+        self.y = [random.uniform(0, 1) for _ in self.x]
 
     def toggle_timer(self):
         if self.timer.isActive():
             self.timer.stop()
             self.ui.pushButton.setText("Start")
         else:
+            self.remaining_time = 10
             self.ui.label_timer.setText(f"{self.remaining_time} seconds")
             self.timer.start()
             self.ui.pushButton.setText("Stop")
+
+    def update_timer_interval(self):
+        interval_value = self.ui.interval.value()
+        self.timer.setInterval(interval_value)
+
+    def update_max_xaxis(self):
+        self.max_x = self.ui.maxxaxis.value()
+        self.x = list(range(1,self.max_x))
+
 
 if __name__ == "__main__":
     app = QApplication([])
